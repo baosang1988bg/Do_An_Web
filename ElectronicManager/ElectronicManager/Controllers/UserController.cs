927dcd40efaa1ce9de193a6fadd799da98523200
sangpls <nguyenbaosang1998@gmail.com>
2019-11-24 22:52:49 +0700

    chắc chưa xong

--- ElectronicManager/ElectronicManager/Controllers/UserController.cs
+++ ElectronicManager/ElectronicManager/Controllers/UserController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ElectronicManager.Models;
 namespace ElectronicManager.Controllers
 {
     public class UserController : Controller
     {
         //
         // GET: /User/
+
+        dbElecDataContext db = new dbElecDataContext();
 
         public ActionResult Index()
         {
@@ -22,5 +24,35 @@ namespace ElectronicManager.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SingIn(FormCollection col, KhachHang kh)
+        {
+            var ten = col["ten"];
+            var email = col["email"];
+            var tendn = col["user"];
+            var matkhau = col["matkhau"];
+
+            kh.HoTen = ten;
+            kh.Email = email;
+            kh.TaiKhoan = tendn;
+            kh.MatKhau = matkhau;
+
+            db.KhachHangs.InsertOnSubmit(kh);
+            db.SubmitChanges();
+
+            return this.SignIn();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection col) 
+        {
+
+            return View();
+        }
     }
 }
