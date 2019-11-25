9263087132a5634f30225c3ee7c8f385cd932d9b
sangpls <nguyenbaosang1998@gmail.com>
2019-11-25 23:43:21 +0700

    add something

--- ElectronicManager/ElectronicManager/App_Start/RouteConfig.cs
+++ ElectronicManager/ElectronicManager/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ElectronicManager
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "DangKy", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
